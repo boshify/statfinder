77c28216180675a70d822b4bf2cd10d3a7cd0aac
boshify <140970186+boshify@users.noreply.github.com>
2023-08-19 17:49:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,26 @@ st.title("URL Statistics Finder")
 
 url = st.text_input("Enter a URL:")
 
-def fetch_stats_from_google(url):
-    # This function will fetch the top 10 search results from Google for the given URL.
-    # Note: This is a basic example and might not work for all URLs.
-    GOOGLE_SEARCH_URL = "https://www.google.com/search?q=" + url
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(GOOGLE_SEARCH_URL, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    search_results = soup.find_all("div", class_="tF2Cxc", limit=10)
+def fetch_stats_from_google(query):
     stats = []
-    for result in search_results:
-        title = result.find("h3", class_="LC20lb DKV0Md").text
-        link = result.find("a")["href"]
-        stats.append({"stat": title, "link": link})
+    try:
+        GOOGLE_CSE_URL = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            "key": "YOUR_API_KEY",  # Replace with your API key
+            "cx": "YOUR_CSE_ID",   # Replace with your Custom Search Engine ID
+            "q": query
+        }
+        response = requests.get(GOOGLE_CSE_URL, params=params)
+        data = response.json()
+        
+        for item in data.get("items", []):
+            title = item["title"]
+            link = item["link"]
+            stats.append({"stat": title, "link": link})
+    except Exception as e:
+        stats.append({"stat": f"Error: {str(e)}", "link": "#"})
     return stats
+
 
 if url:
     stats = fetch_stats_from_google(url)
