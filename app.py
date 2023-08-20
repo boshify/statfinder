9fe94bb046f24265274ad9f56e972a706b618259
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 00:57:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ OPENAI_API_KEY = secrets["OPENAI_API_KEY"]
 # Initialize OpenAI with your API key
 openai.api_key = OPENAI_API_KEY
 
-
 def extract_headings(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -49,10 +48,10 @@ def evaluate_heading_relevance(heading, level):
         return 0
 
 def rank_headings_for_statistics(headings):
-    # Ensure unique headings
-    unique_headings = list(set(headings))
+    exclude_list = ["Platform", "Footer", "Contact Us", "Learn", "Privacy & Terms"]
+    unique_headings = list(set([heading for heading, level in headings if heading not in exclude_list]))
 
-    scores = [(heading, evaluate_heading_relevance(heading, level)) for heading, level in unique_headings]
+    scores = [(heading, evaluate_heading_relevance(heading, level)) for heading, level in headings if heading not in exclude_list]
     sorted_headings = [item[0] for item in sorted(scores, key=lambda x: x[1], reverse=True)]
     return sorted_headings[:10]
 
@@ -63,6 +62,7 @@ url = st.text_input("Enter a URL:")
 if url:
     headings = extract_headings(url)
     top_headings = rank_headings_for_statistics(headings)
-    st.subheader("Top 10 Headings for Adding Statistics:")  # Using subheader for H2
+    st.write("Top 10 Headings for Adding Statistics:")
     for heading in top_headings:
         st.write(heading)
+
