7a9c3d3c78c16ba9f5687746183e8aada9ef3b65
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 01:02:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,16 @@ def extract_headings(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    headings = []
-    for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
-        headings.append((tag.text.strip(), tag.name))
-    return headings
+    h1_heading = soup.find('h1').text.strip() if soup.find('h1') else None
+    if not h1_heading:
+        h1_heading = soup.title.string if soup.title else None
 
-def evaluate_heading_relevance(heading, level):
+    other_headings = [(tag.text.strip(), tag.name) for tag in soup.find_all(['h2', 'h3', 'h4', 'h5', 'h6'])]
+
+    return h1_heading, other_headings
+
+def evaluate_heading_relevance(heading, level, reference_h1):
     weight = {
-        'h1': 1,
         'h2': 1.2,
         'h3': 1.4,
         'h4': 1.6,
@@ -35,7 +37,7 @@ def evaluate_heading_relevance(heading, level):
         'h6': 2
     }.get(level, 1)
 
-    prompt = f"On a scale of 1 to 10, rate the suitability of the heading '{heading}' for adding a statistic from a reputable source (10 being highly suitable)."
+    prompt = f"Given the main topic '{reference_h1}', on a scale of 1 to 10, rate the suitability of the heading '{heading}' for adding a statistic from a reputable source (10 being highly suitable)."
     response = openai.Completion.create(
       engine="davinci",
       prompt=prompt,
@@ -50,11 +52,11 @@ def evaluate_heading_relevance(heading, level):
     except ValueError:
         return 0
 
-def rank_headings_for_statistics(headings):
+def rank_headings_for_statistics(h1_heading, other_headings):
     # Ensure unique headings
-    unique_headings = list(set(headings))
+    unique_headings = list(set(other_headings))
 
-    scores = [(heading, evaluate_heading_relevance(heading, level)) for heading, level in unique_headings]
+    scores = [(heading, evaluate_heading_relevance(heading, level, h1_heading)) for heading, level in unique_headings]
     sorted_headings = [item[0] for item in sorted(scores, key=lambda x: x[1], reverse=True)]
     return sorted_headings[:10]
 
@@ -63,8 +65,11 @@ st.title("Top 10 Headings for Statistics")
 url = st.text_input("Enter a URL:")
 
 if url:
-    headings = extract_headings(url)
-    top_headings = rank_headings_for_statistics(headings)
-    st.subheader("Top 10 Headings for Adding Statistics:")  # Using subheader for better formatting
-    for heading in top_headings:
-        st.write(heading)
+    h1_heading, other_headings = extract_headings(url)
+    if h1_heading:
+        top_headings = rank_headings_for_statistics(h1_heading, other_headings)
+        st.subheader(f"Top 10 Headings Relevant to '{h1_heading}':")  # Using subheader for better formatting
+        for heading in top_headings:
+            st.write(heading)
+    else:
+        st.write("No h1 heading or title found on the page.")
