facd0245c10ef7dabf7c62df2f312727135d92ca
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 01:27:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ openai.api_key = OPENAI_API_KEY
 
 def extract_content(url):
     headers = {
-        # Your headers here...
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
     }
 
     response = requests.get(url, headers=headers)
