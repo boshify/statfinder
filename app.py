3b3bee05e87c8c18665adb077f0e6228c08faecb
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 15:10:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,28 +7,22 @@ import random
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
 
+# NLTK Downloads
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 
-# Load secrets
+# Secrets and API Initialization
 secrets = st.secrets["secrets"]
 GOOGLE_API_KEY = secrets["GOOGLE_API_KEY"]
 CSE_ID = secrets["CSE_ID"]
 OPENAI_API_KEY = secrets["OPENAI_API_KEY"]
-
-# Initialize OpenAI
 openai.api_key = OPENAI_API_KEY
 
-def extract_keywords(text, num=5):
-    tokens = word_tokenize(text)
-    tagged = pos_tag(tokens)
-    keywords = [word for word, pos in tagged if pos in ['NN', 'NNS', 'NNP', 'NNPS'] and word.lower() not in stopwords.words('english')]
-    return keywords[:num]
-
+# Static Patterns and Messages
 STATISTIC_PATTERNS = [
     re.compile(r'(?P<stat>\d+%?\s?[-–—]\s?\d+%)'),
     re.compile(r'(?P<stat>\b\d{1,3}(?:,\d{3})*(?:\.\d+)?\s*(?:million|billion|trillion)?\b)'),
@@ -36,19 +30,21 @@ STATISTIC_PATTERNS = [
     re.compile(r'(?P<stat>\d+%?\s?(?:of|from|to|between)\s?.+?)\.')
 ]
 
-def chunk_text(text, max_length=1500):
-    sentences = text.split('.')
-    chunks = []
-    chunk = ""
-    for sentence in sentences:
-        if len(chunk) + len(sentence) < max_length:
-            chunk += sentence + "."
-        else:
-            chunks.append(chunk)
-            chunk = sentence + "."
-    if chunk:
-        chunks.append(chunk)
-    return chunks
+FUN_MESSAGES = [
+    "Calculating all the pixels...",
+    "Finding the best statistics...",
+    "Analyzing the content...",
+    "Thinking really hard...",
+    "Grabbing some coffee...",
+    "Doing some cool AI stuff..."
+]
+
+# Helper Functions
+def extract_keywords(text, num=5):
+    tokens = word_tokenize(text)
+    tagged = pos_tag(tokens)
+    keywords = [word for word, pos in tagged if pos in ['NN', 'NNS', 'NNP', 'NNPS'] and word.lower() not in stopwords.words('english')]
+    return keywords[:num]
 
 def stylish_box(content):
     box_style = """
@@ -63,7 +59,76 @@ def stylish_box(content):
     """
     return box_style.format(content=content)
 
-# Styling
+def get_webpage_content(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        return response.text if response.status_code == 200 else None
+    except:
+        return None
+
+def extract_content_from_html(html_content):
+    if not html_content:
+        return ""
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    for script in soup(["script", "style"]):
+        script.extract()
+    return " ".join(soup.stripped_strings)
+
+def show_loading_message(duration=6):
+    with st.empty():
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            message = random.choice(FUN_MESSAGES)
+            st.write(message)
+            time.sleep(2)
+
+def search_google(query):
+    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CSE_ID}"
+    response = requests.get(url).json()
+    return response['items'][0]['link']
+
+def score_statistic(statistic, keywords):
+    score = 0
+    for keyword in keywords:
+        score += statistic.lower().count(keyword.lower())
+    return score
+
+def extract_statistic_from_url(url, keywords):
+    page_content = get_webpage_content(url)
+    if not page_content:
+        return []
+
+    page_text = extract_content_from_html(page_content)
+    potential_statistics = []
+
+    for pattern in STATISTIC_PATTERNS:
+        matches = re.finditer(pattern, page_text)
+        for match in matches:
+            surrounding_text = page_text[match.start():match.end()].strip()
+            if surrounding_text and surrounding_text[-1] == '.':
+                potential_statistics.append((surrounding_text, score_statistic(surrounding_text, keywords)))
+
+    potential_statistics.sort(key=lambda x: x[1], reverse=True)
+    return [(stat[0], search_google(stat[0])) for stat in potential_statistics[:10]]
+
+def process_url(url):
+    content = get_webpage_content(url)
+    if not content:
+        return []
+
+    text = extract_content_from_html(content)
+    if not text:
+        return []
+
+    main_keywords = extract_keywords(text)
+    statistics = extract_statistic_from_url(url, main_keywords)
+    return statistics
+
+# UI Styling
 st.markdown(
     """
     <style>
@@ -86,103 +151,10 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# App Execution
 st.title("StatFinder")
 st.write("Enter a URL and find statistics you can link to quickly!")
 url = st.text_input("Enter URL:")
-
-fun_messages = [
-    "Calculating all the pixels...",
-    "Finding the best statistics...",
-    "Analyzing the content...",
-    "Thinking really hard...",
-    "Grabbing some coffee...",
-    "Doing some cool AI stuff..."
-]
-
-def get_webpage_content(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    try:
-        response = requests.get(url, headers=headers)
-        return response.text if response.status_code == 200 else None
-    except:
-        return None
-
-def is_html(content):
-    return content is not None and any(tag in content.lower() for tag in ['<html', '<body', '<head', '<script', '<div', '<span', '<a'])
-
-def extract_content_from_html(html_content):
-    if not is_html(html_content):
-        return ""
-    
-    try:
-        soup = BeautifulSoup(html_content, 'html.parser')
-    except Exception:
-        soup = BeautifulSoup(html_content, 'lxml')
-
-    for script in soup(["script", "style"]):
-        script.extract()
-    return " ".join(soup.stripped_strings)
-
-def show_loading_message(duration=6):
-    with st.empty():  # Use st.empty() to update the placeholder's content
-        start_time = time.time()
-        while time.time() - start_time < duration:
-            message = random.choice(fun_messages)
-            st.write(message)
-            time.sleep(2)
-
-def is_valid_content(sentence):
-    return len(sentence.split()) >= 5 and all(symbol not in sentence for symbol in ['{', '}', '%', '='])
-
-def search_google(query):
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CSE_ID}"
-    response = requests.get(url).json()
-    return response['items'][0]['link']
-
-def score_statistic(statistic, keywords):
-    score = 0
-    for keyword in keywords:
-        score += statistic.lower().count(keyword.lower())
-    return score
-
-def extract_statistic_from_url(url, keywords):
-    page_content = get_webpage_content(url)
-    if not page_content:
-        return []
-    page_text = extract_content_from_html(page_content)
-    
-    potential_statistics = []
-
-    for pattern in STATISTIC_PATTERNS:
-        matches = re.finditer(pattern, page_text)
-        for match in matches:
-            start_idx = match.start()
-            end_idx = match.end()
-
-            start_sentence_idx = page_text.rfind('.', 0, start_idx) + 1  # start of the sentence
-            end_sentence_idx = page_text.find('.', end_idx)  # end of the sentence
-            surrounding_text = page_text[start_sentence_idx:end_sentence_idx + 1].strip()
-
-            if surrounding_text and surrounding_text[-1] == '.' and is_valid_content(surrounding_text):
-                potential_statistics.append((surrounding_text.strip(), score_statistic(surrounding_text, keywords)))
-
-    potential_statistics.sort(key=lambda x: x[1], reverse=True)
-    return [(stat[0], search_google(stat[0])) for stat in potential_statistics[:10]]
-
-def process_url(url):
-    content = get_webpage_content(url)
-    if not content:
-        return []
-
-    text = extract_content_from_html(content)
-    if not text:
-        return []
-
-    main_keywords = extract_keywords(text)
-    statistics = extract_statistic_from_url(url, main_keywords)
-    return statistics
 
 if url:
     show_loading_message()
