8f2ddf6381eb784f474ca6945088daeaf74bb644
boshify <140970186+boshify@users.noreply.github.com>
2023-08-19 20:17:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import openai
 import requests
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
+from urllib.parse import urlparse
 
 # Set up the OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
@@ -12,61 +13,69 @@ GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 CSE_ID = st.secrets["CSE_ID"]
 
 def extract_content_from_url(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p')
-        content = ' '.join([p.text for p in paragraphs])
-        return content
-    except Exception as e:
-        st.write(f"Error in extracting content: {e}")
-        return ""
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.find_all('p')
+    content = ' '.join([p.text for p in paragraphs])
+    return content
 
 def summarize_text_with_gpt(text):
-    try:
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=f"Summarize the following text:\n\n{text}",
-            max_tokens=150
-        )
-        return response.choices[0].text.strip()
-    except Exception as e:
-        st.write(f"Error in summarizing text: {e}")
-        return ""
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Summarize the following text:\n\n{text}",
+        max_tokens=150
+    )
+    return response.choices[0].text.strip()
 
 def generate_queries_with_gpt(text):
-    try:
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=f"Generate 5 statistical queries based on the following summary:\n\n{text}",
-            max_tokens=150,
-            n=5
-        )
-        return [choice.text.strip() for choice in response.choices]
-    except Exception as e:
-        st.write(f"Error in generating queries: {e}")
-        return []
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Generate 5 statistical queries based on the following summary:\n\n{text}",
+        max_tokens=150,
+        n=5
+    )
+    return [choice.text.strip() for choice in response.choices]
 
 def fetch_statistics(queries):
-    try:
-        service = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
-        results = []
-        for query in queries:
-            result = service.cse().list(q=query, cx=CSE_ID).execute()
-            if 'items' in result:
-                for item in result['items']:
-                    results.append({
-                        'title': item['title'],
-                        'link': item['link']
-                    })
-        return results
-    except Exception as e:
-        st.write(f"Error in fetching statistics: {e}")
-        return []
+    service = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
+    results = []
+    for query in queries:
+        result = service.cse().list(q=query, cx=CSE_ID).execute()
+        if 'items' in result:
+            for item in result['items']:
+                results.append({
+                    'title': item['title'],
+                    'link': item['link']
+                })
+    # Ensure unique sources
+    unique_links = set()
+    unique_stats = []
+    for stat in results:
+        if stat['link'] not in unique_links:
+            unique_links.add(stat['link'])
+            unique_stats.append(stat)
+    return unique_stats[:10]  # Limit to 10 statistics
+
+def get_trust_score(stat_title):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Rate the trustworthiness of the following statistic on a scale of 1 to 10:\n\n{stat_title}\n\n",
+        max_tokens=5
+    )
+    score = response.choices[0].text.strip()
+    return f"{score}/10"
+
+def generate_example_use(stat_title, stat_link):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Create a natural sentence using the following statistic:\n\n{stat_title}\n\n",
+        max_tokens=100
+    )
+    sentence = response.choices[0].text.strip()
+    return f"{sentence} [source]({stat_link})"
 
 # Streamlit UI
 st.title("URL Statistics Enhancer")
-
 url = st.text_input("Insert the URL you want to enhance with statistics:")
 
 if url:
@@ -93,4 +102,7 @@ if url:
     for stat in stats:
         st.write("Statistic:", stat['title'])
         st.write("Statistic URL:", stat['link'])
-        st.write("Example Use: . According to a recent study, [source](", stat['link'], ")")
+        trust_score = get_trust_score(stat['title'])
+        st.write("Trust Score:", trust_score)
+        example_use = generate_example_use(stat['title'], stat['link'])
+        st.write("Example Use:", example_use)
