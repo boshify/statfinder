39ecda5f00930470d3627950cf6ebb388942f08c
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 00:46:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,34 +23,29 @@ def extract_headings(url):
     headings = [tag.text.strip() for tag in soup.find_all(['h1', 'h2', 'h3'])]
     return headings
 
-def rank_headings_for_statistics(headings):
-    prompt = "Given the following headings from an article, rank the top 10 headings where adding a statistic from a reputable source would be most beneficial:\n"
-    for idx, heading in enumerate(headings):
-        prompt += f"{idx + 1}. {heading}\n"
-    prompt += "\nRank the headings by number (e.g., 1, 3, 5):"
-
+def evaluate_heading_relevance(heading):
+    prompt = f"How suitable is the heading '{heading}' for adding a statistic from a reputable source? Rate on a scale of 1 to 10 (10 being highly suitable)."
     response = openai.Completion.create(
       engine="davinci",
       prompt=prompt,
-      max_tokens=50,  # Limiting the number of tokens
+      max_tokens=5,
       n=1,
       stop=None,
       temperature=0.5
     )
+    score = response.choices[0].text.strip()
+    try:
+        return int(score)
+    except ValueError:
+        return 0
 
-    st.write("Model's Output:")  # Debugging line
-    st.write(response.choices[0].text.strip())  # Debugging line
+def rank_headings_for_statistics(headings):
+    scores = [(heading, evaluate_heading_relevance(heading)) for heading in headings]
+    # Sort by scores in descending order
+    sorted_headings = [item[0] for item in sorted(scores, key=lambda x: x[1], reverse=True)]
+    return sorted_headings[:10]
 
-    ranked_output = response.choices[0].text.strip().split(",")  # Assuming the model returns a comma-separated list of numbers
-    ranked_headings = []
-    for index_str in ranked_output:
-        index_str = index_str.strip()
-        if index_str.isdigit():
-            index = int(index_str) - 1
-            if 0 <= index < len(headings):
-                ranked_headings.append(headings[index])
 
-    return ranked_headings[:10]
 
 
 
