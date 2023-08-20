c450325cb562a8f4820d07687c615b656f97d08e
boshify <140970186+boshify@users.noreply.github.com>
2023-08-19 20:43:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,114 +1,133 @@
 import streamlit as st
-import openai
 import requests
+import openai
 from bs4 import BeautifulSoup
-from googleapiclient.discovery import build
+import re
 
-# Set up the OpenAI API key
-openai.api_key = st.secrets["secrets"]["OPENAI_API_KEY"]
+# Set Streamlit page configuration
+st.set_page_config(
+    page_title="URL Statistics Enhancer",
+    page_icon="🔍",
+    layout="wide",
+)
 
-# Set up the Google Custom Search API credentials
-GOOGLE_API_KEY = st.secrets["secrets"]["GOOGLE_API_KEY"]
-CSE_ID = st.secrets["secrets"]["CSE_ID"]
+# Fetch secrets
+API_KEY = st.secrets["GOOGLE_API_KEY"]
+CSE_ID = st.secrets["CSE_ID"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+openai.api_key = OPENAI_API_KEY
 
-def extract_content_from_url(url):
+def extract_text_from_url(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 403:
-            return "403 Forbidden"
-        soup = BeautifulSoup(response.content, 'html.parser')
-        tags = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li'])
-        content = ' '.join([tag.text for tag in tags])
-        return content
-    except Exception as e:
-        st.write(f"Error in extracting content: {e}")
-        return ""
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Extract text from H1 to start of footer
+    h1 = soup.find('h1')
+    footer = soup.find('footer')
+    if h1 and footer:
+        content = ''.join(str(item) for item in h1.find_all_next() if item != footer)
+        return BeautifulSoup(content, 'html.parser').get_text()
+    return None
 
 def summarize_text_with_gpt(text):
     try:
         response = openai.Completion.create(
             engine="davinci",
-            prompt=f"Provide a concise summary of the following content:\n\n{text}",
+            prompt=f"Summarize the following content:\n\n{text}",
             max_tokens=300
         )
         return response.choices[0].text.strip()
     except Exception as e:
-        st.write(f"Error in summarizing text: {e}")
-        return ""
+        st.error(f"Error in summarize_text_with_gpt: {e}")
+        return text
 
 def generate_queries_with_gpt(text):
     try:
         response = openai.Completion.create(
             engine="davinci",
-            prompt=f"Generate 5 statistical queries based on the following summary:\n\n{text}",
-            max_tokens=150,
-            n=5
+            prompt=f"From the summarized content, generate 10 sentences where a statistic should be added:\n\n{text}",
+            max_tokens=300,
+            n=10
         )
-        return [choice.text.strip() for choice in response.choices]
+        queries = [choice.text.strip() for choice in response.choices]
+        return queries
     except Exception as e:
-        st.write(f"Error in generating queries: {e}")
+        st.error(f"Error in generate_queries_with_gpt: {e}")
         return []
 
-def fetch_statistics(queries):
-    try:
-        service = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
-        results = []
-        for query in queries:
-            result = service.cse().list(q=query, cx=CSE_ID).execute()
-            if 'items' in result:
-                for item in result['items']:
-                    results.append({
-                        'title': item['title'],
-                        'link': item['link']
-                    })
-        return results
-    except Exception as e:
-        st.write(f"Error in fetching statistics: {e}")
-        return []
+def fetch_stat_from_google(query):
+    GOOGLE_CSE_URL = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        'q': f"statistics about {query}",
+        'key': API_KEY,
+        'cx': CSE_ID,
+    }
+    response = requests.get(GOOGLE_CSE_URL, params=params)
+    data = response.json()
+    results = []
+    if data.get("items"):
+        for item in data["items"]:
+            # Ensure the statistic contains a number
+            if any(char.isdigit() for char in item["snippet"]):
+                results.append({
+                    "stat": item["snippet"],
+                    "link": item["link"]
+                })
+    return results
 
-def generate_example_use_with_gpt(statistic_title, statistic_url):
+def get_trust_score_with_gpt(stat):
     try:
         response = openai.Completion.create(
             engine="davinci",
-            prompt=f"Create a natural sentence using the following statistic title: \"{statistic_title}\" and its source: \"{statistic_url}\".",
-            max_tokens=150
+            prompt=f"Score this information from 1-10 on a scale of believability. 10 being highly believable and 1 being hardest to believe:\n\n{stat}",
+            max_tokens=10
         )
-        return response.choices[0].text.strip()
+        score = int(response.choices[0].text.strip())
+        return score
     except Exception as e:
-        st.write(f"Error in generating example use: {e}")
-        return "Error"
+        st.error(f"Error in get_trust_score_with_gpt: {e}")
+        return 5  # Default score
 
-# Streamlit UI
-st.title("URL Statistics Enhancer")
-url = st.text_input("Insert the URL you want to enhance with statistics:")
+# Streamlit layout and components
+c30, c31 = st.columns([10.5, 1])
+
+with c30:
+    st.title("URL Statistics Enhancer")
+
+st.markdown("----")
+
+url = st.text_input('Insert the URL you want to enhance with statistics:')
 
 if url:
-    st.write("URL provided:", url)
+    text = extract_text_from_url(url)
+    if text:
+        summarized_text = summarize_text_with_gpt(text[:2000])  # Limiting the text to the first 2000 characters
+        st.write(f"**Summarized Text:**\n\n{summarized_text[:500]}...")  # Displaying the first 500 characters
+        st.markdown("---")
+        queries = generate_queries_with_gpt(summarized_text)
+        displayed_links = set()  # To avoid repeated websites
+        count = 0
+        for query in queries:
+            stats_data = fetch_stat_from_google(query)
+            for stat_data in stats_data:
+                if stat_data["link"] not in displayed_links:
+                    displayed_links.add(stat_data["link"])
+                    trust_score = get_trust_score_with_gpt(stat_data["stat"])
+                    st.markdown(f"**Statistic:** {stat_data['stat']}")
+                    st.markdown(f"**Statistic URL:** [{stat_data['link']}]({stat_data['link']})")
+                    st.markdown(f"**Trust Score:** {trust_score}/10")
+                    example_use = re.sub(r'\d+', '', query)  # Remove numbers
+                    st.markdown(f"**Example Use:** `{example_use} [source]({stat_data['link']})`")
+                    st.markdown("---")
+                    count += 1
+                    if count >= 10:
+                        break
+            if count >= 10:
+                break
+    else:
+        st.error("Unable to extract content from the provided URL.")
 
-    # Extract and summarize the content
-    extracted_content = extract_content_from_url(url)
-    st.write("Extracted Content:", extracted_content[:1000] + "...")  # Displaying the first 1000 characters for brevity
-
-    summarized_text = summarize_text_with_gpt(extracted_content)
-    st.write("Summarized Text:", summarized_text)
-
-    # Generate queries for statistics
-    queries = generate_queries_with_gpt(summarized_text)
-    st.write("Generated Queries:", queries)
-
-    # Fetch statistics based on the queries
-    stats = fetch_statistics(queries)
-
-    # Display the results
-    st.write("Results:")
-    for i, stat in enumerate(stats[:10]):  # Limiting to 10 results
-        example_use = generate_example_use_with_gpt(stat['title'], stat['link'])
-        
-        st.write(f"Statistic {i+1}:", stat['title'])
-        st.write("Statistic URL:", stat['link'])
-        st.write("Example Use:", example_use)
-        if st.button("Copy to Clipboard", key=f"copy_{i}"):
-            st.copied_text(stat['title'] + "\n" + stat['link'] + "\n" + example_use)
+st.markdown("----")
