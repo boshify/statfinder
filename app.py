cc4eed91068d35c10895c3713fa005439e59b3ff
boshify <140970186+boshify@users.noreply.github.com>
2023-08-19 18:46:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,10 @@ openai.api_key = OPENAI_API_KEY
 
 def extract_text_from_url(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
         
         # Extract text from H1 to start of footer
@@ -33,6 +36,7 @@ def extract_text_from_url(url):
     except Exception as e:
         st.error(f"Error in extract_text_from_url: {e}")
         return None
+
 
 def generate_queries_with_gpt(text):
     try:
