22187ba22d9fec8d3fabbfc8d171686798ac4c53
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 18:45:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ import re
 
 # Regex patterns to identify sentences with potential statistics
 STATISTIC_PATTERNS = [
-    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?%',  # Matches percentages like '78%' or '1,234.56%'
+    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?%',  # Matches percentages
     r'1 in \d+',                      # Matches '1 in 10'
     r'1 of \d+',                      # Matches '1 of 10'
-    r'\$\d{1,3}(?:,\d{3})*(?:\.\d+)?',  # Matches dollar values like '$111' or '$1,234,567.89'
-    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?',    # Matches decimal and non-decimal numbers
+    r'\$\d{1,3}(?:,\d{3})*(?:\.\d+)?',  # Matches dollar values
+    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?'    # Matches decimal and non-decimal numbers
 ]
 
-def stylish_box(content):
-    box_style = """
+def stylish_box(statistic, url):
+    box_content = f"""
+    <div>
+        <strong>Statistic:</strong> {statistic}<br>
+        <strong>URL:</strong> <a href="{url}" target="_blank">{url}</a><br>
+        <strong>Example:</strong> In a recent discussion, I mentioned that '{statistic}'.
+    </div>
+    """
+    box_style = f"""
     <div style="
         border: 2px solid #f1f1f1;
         border-radius: 5px;
         padding: 10px;
         margin: 10px 0px;
         box-shadow: 2px 2px 12px #aaa;">
-        {content}
+        {box_content}
     </div>
     """
-    return box_style.format(content=content)
+    return box_style
 
 def get_webpage_content(url):
     headers = {
@@ -41,25 +48,24 @@ def extract_content_from_html(html_content):
         script.extract()
     return " ".join(soup.stripped_strings)
 
-def extract_statistics(text_content):
+def extract_statistics(text_content, url):
     results = []
     for pattern in STATISTIC_PATTERNS:
         matches = re.findall(pattern, text_content)
         for match in matches:
             start_idx = text_content.find(match)
-            surrounding_text = text_content[max(0, start_idx - 60):min(start_idx + len(match) + 60, len(text_content))]
+            surrounding_text = text_content[max(0, start_idx - 30):min(start_idx + len(match) + 30, len(text_content))]
             results.append(surrounding_text.strip())
-    return results
+    return [stylish_box(statistic, url) for statistic in results[:10]]
 
 def process_url(url):
     html_content = get_webpage_content(url)
     if html_content:
         text_content = extract_content_from_html(html_content)
-        statistics = extract_statistics(text_content)
+        statistics_boxes = extract_statistics(text_content, url)
         
-        for idx, statistic in enumerate(statistics[:10], 1):
-            content = f"{idx}. {statistic}"
-            st.markdown(stylish_box(content), unsafe_allow_html=True)
+        for box in statistics_boxes:
+            st.markdown(box, unsafe_allow_html=True)
     else:
         st.error("Unable to fetch the content from the provided URL. Please check if the URL is correct and try again.")
 
