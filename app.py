7af844211c4d2d948c2c185850cffa81fd12ee2d
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 18:32:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,108 +1,44 @@
-import streamlit as st
-import openai
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import re
 
-# Load secrets
-secrets = st.secrets["secrets"]
-GOOGLE_API_KEY = secrets["GOOGLE_API_KEY"]
-CSE_ID = secrets["CSE_ID"]
-OPENAI_API_KEY = secrets["OPENAI_API_KEY"]
+def get_content_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.find_all('p')
+    content = [p.get_text() for p in paragraphs if p.get_text()]
+    return content
 
-# Initialize OpenAI
-openai.api_key = OPENAI_API_KEY
+def is_statistic(text):
+    if re.search(r'\d', text):
+        return True
+    return False
 
-def chunk_text(text, max_length=1500):
-    sentences = text.split('.')
-    chunks = []
-    chunk = ""
-    for sentence in sentences:
-        if len(chunk) + len(sentence) < max_length:
-            chunk += sentence + "."
-        else:
-            chunks.append(chunk)
-            chunk = sentence + "."
-    if chunk:
-        chunks.append(chunk)
-    return chunks
+def format_output(data_list):
+    output = "**StatGrabber**\n"
+    output += "Enter a URL and find statistics you can link to quickly!\n\n"
+    output += f"Enter URL:\n[{url}]({url})\n\n"
+    
+    for idx, (text, url) in enumerate(data_list, 1):
+        if is_statistic(text):
+            output += f"{idx}.\n"
+            output += f"#### Statistic:\n"
+            output += f"{text}\n\n"
+            output += f"#### Example:\n"
+            output += f"In a recent discussion on cryptocurrencies, I mentioned that '{text}'.\n\n"
+            output += f"#### URL:\n"
+            output += f"[{url}]({url})\n\n"
+    
+    return output
 
-def get_webpage_content(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    try:
-        response = requests.get(url, headers=headers)
-        return response.text if response.status_code == 200 else None
-    except:
-        return None
+# Use the provided URL
+url = "https://cointelegraph.com/news/mineflation-cost-to-mine-one-bitcoin-in-the-us-rises-from-5k-to-17k-in-2023"
+content = get_content_from_url(url)
 
-def extract_content_from_html(html_content):
-    soup = BeautifulSoup(html_content, 'lxml')
-    for script in soup(["script", "style"]):
-        script.extract()
-    return " ".join(soup.stripped_strings)
+# Assuming URLs are extracted from the same page, using a placeholder list of URLs for this example
+urls = [url for _ in content]
 
-def search_google(query):
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CSE_ID}"
-    response = requests.get(url).json()
-    if 'items' in response:
-        for item in response['items']:
-            link = item.get('link')
-            if link:
-                return link
-    return None
+# Pair each content piece with its URL
+extracted_data = list(zip(content, urls))
 
-def extract_statistic_from_url(stat_url):
-    page_content = get_webpage_content(stat_url)
-    if not page_content:
-        return None, stat_url
-    page_text = extract_content_from_html(page_content)
-    # Truncate while ensuring the sentence doesn't cut off abruptly
-    sentences = page_text.split('.')
-    truncated_text = ".".join(sentences[:2])
-    return truncated_text, stat_url
-
-def process_url(main_url):
-    html_content = get_webpage_content(main_url)
-    if html_content:
-        text_content = extract_content_from_html(html_content)
-        chunks = chunk_text(text_content)
-        aggregated_points = []
-        
-        for text_chunk in chunks:
-            response = openai.Completion.create(
-                engine="davinci",
-                prompt=f"Provide concise summaries for the main ideas in the following content:\n{text_chunk}",
-                max_tokens=150
-            )
-            key_points = response.choices[0].text.strip().split("\n")
-            aggregated_points.extend(key_points)
-            
-        for idx, point in enumerate(aggregated_points[:10], 1):
-            search_query = f"statistics related to {point}"
-            stat_url = search_google(search_query)
-            
-            if stat_url:
-                statistic, _ = extract_statistic_from_url(stat_url)
-            else:
-                statistic = None
-
-            if statistic:
-                content = f"<h4>{idx}. {point}</h4><br><h4>Statistic:</h4> <p>{statistic}</p><h4>Example:</h4> <p>In a recent discussion on cryptocurrencies, I mentioned that '{statistic}'.</p><h4>URL:</h4> <p>{stat_url}</p>"
-            else:
-                content = f"<h4>{idx}. {point}</h4><br>No relevant statistic found."
-            st.markdown(content, unsafe_allow_html=True)
-    else:
-        st.error("Unable to fetch the content from the provided URL. Please check if the URL is correct and try again.")
-
-
-st.title("StatGrabber")
-st.write("Enter a URL and find statistics you can link to quickly!")
-url = st.text_input("Enter URL:")
-
-if url:
-    process_url(url)
-
-st.sidebar.header("About")
-st.sidebar.write("StatGrabber is an AI-powered tool to help you quickly find and cite statistics related to your article or content.")
-st.sidebar.write("This tool uses GPT-3.5-turbo and other AI models to analyze the content and fetch relevant statistics.")
+print(format_output(extracted_data))
