6465cbd7dd646484b5b2a9c6e37ba5ec86ec871a
boshify <140970186+boshify@users.noreply.github.com>
2023-08-20 18:04:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,14 @@ openai.api_key = OPENAI_API_KEY
 
 # Regex patterns to identify sentences with potential statistics
 STATISTIC_PATTERNS = [
-    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?%',  # Matches percentages
-    r'1 in \d+',                      # Matches '1 in 10'
-    r'1 of \d+',                      # Matches '1 of 10'
-    r'\$\d{1,3}(?:,\d{3})*(?:\.\d+)?',  # Matches dollar values
-    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?',    # Matches decimal and non-decimal numbers
+    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?%',  # Percentages
+    r'1 in \d+',                      # '1 in 10'
+    r'1 of \d+',                      # '1 of 10'
+    r'\$\d{1,3}(?:,\d{3})*(?:\.\d+)?',  # Dollar values
+    r'\d{1,3}(?:,\d{3})*(?:\.\d+)?',    # Decimal and non-decimal numbers
 ]
 
 def chunk_text(text, max_length=1500):
-    """Breaks the text into chunks that are less than the specified max length."""
     sentences = text.split('.')
     chunks = []
     chunk = ""
@@ -40,7 +39,6 @@ def chunk_text(text, max_length=1500):
     return chunks
 
 def stylish_box(content):
-    """Styles a content box."""
     box_style = """
     <div style="
         border: 2px solid #f1f1f1;
@@ -53,46 +51,13 @@ def stylish_box(content):
     """
     return box_style.format(content=content)
 
-# Styling
-st.markdown(
-    """
-    <style>
-        .reportview-container {
-            background: black;
-            color: white;
-        }
-        h1 {
-            font-size: 50px;
-            margin-bottom: 30px;
-        }
-        img {
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
-            margin-top: 30px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 st.title("StatGrabber")
 st.write("Enter a URL and find statistics you can link to quickly!")
 url = st.text_input("Enter URL:")
 
-fun_messages = [
-    "Calculating all the pixels...",
-    "Finding the best statistics...",
-    "Analyzing the content...",
-    "Thinking really hard...",
-    "Grabbing some coffee...",
-    "Doing some cool AI stuff..."
-]
-
 def get_webpage_content(url):
-    """Fetches the content from a URL."""
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     try:
         response = requests.get(url, headers=headers)
@@ -100,83 +65,79 @@ def get_webpage_content(url):
     except:
         return None
 
+def is_html(content):
+    return content is not None and any(tag in content.lower() for tag in ['<html', '<body', '<head', '<script', '<div', '<span', '<a'])
+
 def extract_content_from_html(html_content):
-    """Extracts meaningful content from HTML."""
-    soup = BeautifulSoup(html_content, 'lxml')
+    if not is_html(html_content):
+        return ""
+    
+    try:
+        soup = BeautifulSoup(html_content, 'html.parser')
+    except Exception:
+        soup = BeautifulSoup(html_content, 'lxml')
+
     for script in soup(["script", "style"]):
         script.extract()
     return " ".join(soup.stripped_strings)
 
-def show_loading_message(duration=6):
-    """Shows a loading message to the user for the specified duration."""
-    loading_message_placeholder = st.empty()
-    start_time = time.time()
-    while time.time() - start_time < duration:
-        loading_message_placeholder.text(random.choice(fun_messages))
-        time.sleep(2)
-
-def search_google(query):
-    """Uses Google Custom Search to find a related link."""
+def search_google(query, exclude_url):
     url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CSE_ID}"
     response = requests.get(url).json()
     if 'items' in response:
-        return response['items'][0]['link']
+        for item in response['items']:
+            link = item.get('link')
+            if link and link != exclude_url:
+                return link
     return None
 
-def extract_statistic_from_url(url):
-    """Extracts a statistic from a URL."""
-    page_content = get_webpage_content(url)
+def extract_statistic_from_url(stat_url):
+    page_content = get_webpage_content(stat_url)
     if not page_content:
-        return None, url
+        return None, stat_url
     page_text = extract_content_from_html(page_content)
+    
     for pattern in STATISTIC_PATTERNS:
         matches = re.findall(pattern, page_text)
         for match in matches:
             start_idx = page_text.find(match)
             surrounding_text = page_text[max(0, start_idx - 60):min(start_idx + len(match) + 60, len(page_text))]
-            return surrounding_text.strip(), url
-    return None, url
+            surrounding_text = surrounding_text.strip()
+            if len(surrounding_text.split()) > 5:
+                return surrounding_text, stat_url
+    return None, stat_url
 
-def process_url(url):
-    """Processes a URL to extract and summarize its content."""
-    with st.spinner():
-        show_loading_message()
-        html_content = get_webpage_content(url)
-        if html_content:
-            text_content = extract_content_from_html(html_content)
-            chunks = chunk_text(text_content)
+def process_url(main_url):
+    html_content = get_webpage_content(main_url)
+    if html_content:
+        text_content = extract_content_from_html(html_content)
+        chunks = chunk_text(text_content)
+        aggregated_points = []
+        
+        for text_chunk in chunks:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=f"Provide concise summaries for the main ideas in the following content:\n{text_chunk}",
+                max_tokens=150
+            )
+            key_points = response.choices[0].text.strip().split("\n")
+            aggregated_points.extend(key_points)
             
-            progress = st.progress(0)
-            total_chunks = len(chunks)
-            aggregated_points = []
-            
-            for idx, text_chunk in enumerate(chunks):
-                response = openai.ChatCompletion.create(  # Corrected to use ChatCompletion
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "user", "content": f"Provide concise summaries for the main ideas in the following content:\n{text_chunk}"}
-                    ]
-                )
-                key_points = response.choices[0]["message"]["content"].strip().split("\n")
-                aggregated_points.extend(key_points)
-                progress.progress((idx+1)/total_chunks)
-            
-            for idx, point in enumerate(aggregated_points[:10], 1):
-                search_query = f"statistics 2023 {point}"
-                google_result = search_google(search_query)
-                if google_result:
-                    statistic, stat_url = extract_statistic_from_url(google_result)
-                else:
-                    statistic, stat_url = None, None
-                
-                if statistic:
-                    content = f"{idx}. {point}<br><br>Statistic: {statistic}<br>URL: {stat_url}<br><button onclick=\"navigator.clipboard.writeText('{statistic} - Source: {stat_url}')\">Copy to clipboard</button>"
-                    st.markdown(stylish_box(content), unsafe_allow_html=True)
-                else:
-                    content = f"{idx}. {point}<br><br>No relevant statistic found."
-                    st.markdown(stylish_box(content), unsafe_allow_html=True)
-        else:
-            st.error("Unable to fetch the content from the provided URL. Please check if the URL is correct and try again.")
+        for idx, point in enumerate(aggregated_points[:10], 1):
+            search_query = f"statistics 2023 {point}"
+            stat_url = search_google(search_query, main_url)
+            if stat_url:
+                statistic, _ = extract_statistic_from_url(stat_url)
+            else:
+                statistic = None
+
+            if statistic:
+                content = f"<h3>{idx}. {point}</h3><br>Statistic: {statistic}<br>Example: In a recent discussion on cryptocurrencies, I mentioned that '{statistic}'.<br><br>URL: {stat_url}"
+            else:
+                content = f"<h3>{idx}. {point}</h3><br>No relevant statistic found."
+            st.markdown(stylish_box(content), unsafe_allow_html=True)
+    else:
+        st.error("Unable to fetch the content from the provided URL. Please check if the URL is correct and try again.")
 
 if url:
     process_url(url)
